1350a4d7bee4d22c8f54ad6808593c8539f6d9bb
unknown <benjoquilario@gmail.com>
2022-12-07 12:40:31 +0800

    add i18n and images domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ['s4.anilist.co'],
+  },
+  i18n: {
+    locales: ['en-US', 'fr', 'nl-NL', 'nl-BE'],
+    defaultLocale: 'en-US',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
