5fdd7583a27f57de4322178d60742670df4006a9
unknown <benjoquilario@gmail.com>
2022-12-07 12:52:14 +0800

    get the next airing time

---
+++ src/utils/time.ts
@@ -0,0 +1,11 @@
+import dayjs from 'dayjs';
+import updateLocale from 'dayjs/plugin/updateLocale';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/vi';
+import 'dayjs/locale/ru';
+import 'dayjs/locale/es';
+
+dayjs.extend(updateLocale);
+dayjs.extend(relativeTime);
+
+export default dayjs;
