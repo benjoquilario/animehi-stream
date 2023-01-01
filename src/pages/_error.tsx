a9b28ebc65dcdc831f3c093a21c972d5120609c8
unknown <benjoquilario@gmail.com>
2023-01-01 14:26:09 +0800

    implement: custom errors

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,14 +1,30 @@
-import { NextSeo } from 'next-seo';
-import Link from 'next/link';
 import React from 'react';
+import { NextPage } from 'next';
+import NextErrorComponent, { ErrorProps } from 'next/error';
+import Link from 'next/link';
+import { NextSeo } from 'next-seo';
 
-const ErrorPage = () => {
+interface CustomErrorProps extends ErrorProps {
+  hasGetInitialPropsRun: boolean;
+  err?: Error & {
+    statusCode: number;
+  };
+}
+
+// @ts-ignore
+const ErrorPage: NextPage<CustomErrorProps, CustomErrorProps> = ({
+  statusCode,
+  err,
+}) => {
   return (
     <React.Fragment>
-      <NextSeo title="An error occured (404)" description="404 Not Found" />
+      <NextSeo
+        title={`An error occured ${statusCode} - AnimeHi`}
+        description="404 Not Found"
+      />
       <div className="relative w-full min-h-screen flex justify-center items-center">
         <div className="fixed z-0 w-full h-full flex items-center justify-center">
-          <h1 className="font-bold text-[30vw] text-gray-500">404</h1>
+          <h1 className="font-bold text-[30vw] text-gray-500">{statusCode}</h1>
           <div className="absolute inset-0 bg-black/90 w-full h-full"></div>
         </div>
         <div className="px-4 md:px-12 lg:px-20 xl:px-28 2xl:px-36 relative z-10 flex flex-col items-center w-full h-full text-center ">
@@ -16,17 +32,14 @@ const ErrorPage = () => {
             <span className="text-lg">
               Welcome to
               <span className="text-primary opacity-300">
-                {' '}
-                the 404 dimension
+                the {statusCode} dimension
               </span>
             </span>
           </div>
           <p className="text-4xl font-semibold text-white">
             You have discovered a new dimension
           </p>
-          <p className="text-2xl text-slate-200 mt-4">
-            But unfortunately, this dimension has nothing at all
-          </p>
+          <p className="text-2xl text-slate-200 mt-4">{`Error: ${err}`}</p>
           <Link href="/">
             <a>
               <button
@@ -43,6 +56,11 @@ const ErrorPage = () => {
       </div>
     </React.Fragment>
   );
+};
+
+// @ts-ignore
+ErrorPage.getInitialProps = async context => {
+  return await NextErrorComponent.getInitialProps(context);
 };
 
 export default ErrorPage;
