cc0f329dd286874b8b95d0e36fcd38c589f85221
benjoquilario <benjoquilario@gmail.com>
2023-09-26 13:57:30 +0800

    move to app dir with next 13

--- src/components/anime/related.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Related = () => {
-  return <div>Related</div>;
-};
-
-export default Related;
--- src/pages/browse.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Browse = () => {
-  return <div>Browse</div>;
-};
-
-export default Browse;
