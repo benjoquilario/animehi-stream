978d25ef95748a087f7cdf26844700c2e93a4058
benjoquilario <benjoquilario@gmail.com>
2024-05-25 19:19:46 +0800

    -

--- src/components/player/oplayer/ssr.tsx
+++ src/components/player/oplayer/ssr.tsx
@@ -19,6 +19,7 @@ export default function VideoPlayer(props: WatchProps) {
     episodeNumber,
     poster,
     title,
+    malId,
   } = props
 
   // const watchHistory = await animeWatchById(animeId)
@@ -32,6 +33,7 @@ export default function VideoPlayer(props: WatchProps) {
       poster={poster}
       anilistId={anilistId}
       title={title}
+      malId={malId}
     />
   )
 }
