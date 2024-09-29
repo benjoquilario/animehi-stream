7b41137e0266d5cfa82ec171c21995e1f2bf0b6b
quilariobenjo <94890755+quilariobenjo@users.noreply.github.com>
2024-09-29 09:44:57 +0000

    feat: make dropdown menu better add callback when user is not logged in user page

--- src/components/auth-form.tsx
+++ src/components/auth-form.tsx
@@ -44,128 +44,80 @@ const AuthForm = () => {
     )
   }
 
-  return (
-    <>
-      {session ? (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button
-              variant="ghost"
-              aria-label="toggle dropdown"
-              className="relative h-8 w-8 rounded-full bg-primary hover:bg-primary/90"
-            >
-              <Avatar className="h-8 w-8">
-                <AvatarImage
-                  src={session.user.image ?? ""}
-                  alt={session.user.name ?? ""}
-                />
-                <AvatarFallback>
-                  <div className="h-full w-full animate-pulse"></div>
-                </AvatarFallback>
-              </Avatar>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent
-            className="mr-4 mt-4 w-64"
-            align="start"
-            forceMount
-          >
-            <DropdownMenuLabel className="font-normal">
-              <div className="flex flex-col space-y-1">
-                <p className="text-sm font-medium leading-none">
-                  {session?.user?.name}
-                </p>
-                <p className="text-xs leading-none text-muted-foreground">
-                  {session?.user.email}
-                </p>
-              </div>
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem asChild>
-                <Link
-                  className="w-full"
-                  href={`/user/${session?.user?.name}/${session.user.id}`}
-                >
-                  Profile
-                </Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                Settings
-                <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem asChild>
-              <button
-                onClick={() => logout()}
-                // variant="ghost"
-                className="relative h-8 w-full justify-start"
-              >
-                Logout
-              </button>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      ) : (
-        <Button size="sm" onClick={() => setIsAuthOpen(true)}>
-          Sign in
-        </Button>
-      )}
+  const clickHandler = async (provider: string) => {
+    try {
+      const res = await loginAnilist(provider)
 
-      <Dialog open={isAuthOpen} onOpenChange={setIsAuthOpen}>
-        <DialogContent className="p-8">
-          {/* <DialogHeader>
-            <DialogTitle>
-              {!isLogin ? "Login" : "Create an account"}
-            </DialogTitle>
-            <DialogDescription>
-              {isLogin
-                ? "Create an account to enjoy more features"
-                : "Enjoy your favourite anime in high quality for free!"}
-            </DialogDescription>
-          </DialogHeader>
-          {!isLogin ? <Login /> : <Register />}
-          <div className="absolute bottom-4 left-6">
-            {!isLogin ? (
-              <>
-                Don&apos;t have an account
-                <button
-                  className="ml-2 text-primary"
-                  onClick={() => setIsLogin(true)}
-                >
-                  Sign Up
-                </button>
-              </>
+      if (res?.success) window.location.reload()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          aria-label="toggle dropdown"
+          className="relative h-8 w-8 rounded-full bg-primary hover:bg-primary/90"
+        >
+          <Avatar className="h-8 w-8">
+            <AvatarImage
+              src={
+                `${session ? session.user.image : "/placeholder.png"}` ??
+                "/placeholder.png"
+              }
+              alt={`${session ? session.user.name : ""}` ?? ""}
+            />
+            <AvatarFallback>
+              <div className="h-full w-full animate-pulse"></div>
+            </AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="mr-4 mt-4 w-64" align="start" forceMount>
+        <DropdownMenuGroup>
+          <DropdownMenuItem asChild>
+            {session ? (
+              <Link
+                className="w-full"
+                href={`/user/${session?.user?.name}/${session.user.id}`}
+              >
+                Profile
+              </Link>
             ) : (
-              <>
-                Already have an account?
-                <button
-                  className="ml-2 text-primary"
-                  onClick={() => setIsLogin(false)}
-                >
-                  Sign In
-                </button>
-              </>
+              <Link className="w-full" href={`/user`}>
+                Profile
+              </Link>
             )}
-          </div> */}
-          <Button
-            disabled={isAnilistLoading}
-            type="button"
-            className="w-full"
-            onClick={() => {
-              startTransitionAnilist(async () => {
-                await loginAnilist().then(() =>
-                  toast.success("Signed in successfully")
-                )
-              })
-            }}
-          >
-            Sign in with Anilist
-          </Button>
-        </DialogContent>
-      </Dialog>
-    </>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Settings
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          {session ? (
+            <button
+              onClick={() => logout()}
+              // variant="ghost"
+              className="relative h-8 w-full justify-start"
+            >
+              Logout
+            </button>
+          ) : (
+            <button
+              onClick={clickHandler.bind(null, "anilist")}
+              className="relative h-8 w-full justify-start"
+            >
+              Sign in
+            </button>
+          )}
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 
