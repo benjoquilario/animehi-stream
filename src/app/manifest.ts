a9c7f201af5c7ddd7ed15da07e65d228a657a40c
benjoquilario <benjoquilario@gmail.com>
2024-09-29 20:41:32 +0800

    change background color for pwa

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -9,8 +9,8 @@ const json = {
   short_name: "AnimeHi",
   description:
     "AnimeHi is an anime streaming service that uses the consumet API. No ads and no vpn required",
-  theme_color: "#FFFFFF",
-  background_color: "#FFFFFF",
+  theme_color: "#071203",
+  background_color: "#071203",
   display: "standalone",
   orientation: "portrait",
   id: "/",
@@ -18,14 +18,14 @@ const json = {
   start_url: "/",
   icons: [
     {
-      src: "/anime-192x192.png",
-      sizes: "192x192",
+      src: "/anime-512x512.png",
+      sizes: "any",
       type: "image/png",
       purpose: "maskable",
     },
     {
       src: "/anime-512x512.png",
-      sizes: "512x512",
+      sizes: "any",
       type: "image/png",
     },
   ],
